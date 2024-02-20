3d86e63271f8a04d87868c7db8a6a98312267e43
Snapshot0010 <snapshot00010@gmail.com>
2024-02-19 23:23:36 -0800

    Added the PlayerScript and TurnMaster scripts and added in some starting variables

---
+++ Crypto Wars/Assets/Scripts/TurnMaster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMaster : MonoBehaviour
+{
+    int currTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currTurn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Test to see if either of the two game end scenarios have been met
+
+        //Controlled End
+
+        //Timer End
+    }
+}
