0908d98218e25923191169a3838e0e409fb1f4b7
zmorgancs <122938637+zmorgancs@users.noreply.github.com>
2024-02-22 20:41:08 -0800

    Feature added: simple WASD camera controls
    Added functionality for WASD user input to control the cameras X and Y axis

---
+++ Crypto Wars/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject gameController;
+    public float cameraSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the game-controller to new game controller
+        gameController = new GameObject("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if keyboard<insert key here> was pressed
+            // access keyboard to move across x and y (up down left right)
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, cameraSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -cameraSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-cameraSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
