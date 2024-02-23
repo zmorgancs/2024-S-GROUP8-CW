b484eca184127eda9def8779b51c5f608a5168d9
Snapshot0010 <snapshot00010@gmail.com>
2024-02-22 17:38:13 -0800

    Finished the scripting for TurnMaster script

--- Crypto Wars/Assets/Scripts/TurnMaster.cs
+++ Crypto Wars/Assets/Scripts/TurnMaster.cs
@@ -5,19 +5,50 @@ using UnityEngine;
 public class TurnMaster : MonoBehaviour
 {
     int currTurn;
+    private float turnTimer;
+    private float maxTurnTime = 30f;
+    Player[] Players = new Player[2];
     // Start is called before the first frame update
     void Start()
     {
         currTurn = 0;
+        turnTimer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Test to see if either of the two game end scenarios have been met
+        // Every frame, check if every player's turn is done or if the max Timer is finished 
+        if(allAreDone(Players) || turnTimer == maxTurnTime)
+        {
+            currTurn += 1;
+            newTurn(Players);
+            turnTimer = 0f;
+        }
+        turnTimer += Time.deltaTime;
+    }
 
-        //Controlled End
+    // Simply iterates through the Players array.
+    // If it every runs into a player's turn that is not finished, it returns false
+    // If it gets through every index and does not return, exits the for loop and returns true
+    bool allAreDone(Player[] Players)
+    {
+        for (int i = 0; i < Players.Length; i++)
+        {
+            if(!Players[i].IsPlayerTurnFinished())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        //Timer End
+    // Iterats through the Players array and sets all of the isDone booleans to false.
+    void newTurn(Player[] Players)
+    {
+        for (int i = 0; i < Players.Length; i++)
+        {
+            Players[i].PlayerStartTurn();
+        }
     }
 }
