b5725e8f36768d797e89440bfed6d3b1612f5a07
Gnarizard <toefoo@protonmail.com>
2024-03-22 15:35:22 -0700

    Updated the cardRegistry script to hold created cards

--- Crypto Wars/Assets/Scripts/cardRegistry.cs
+++ Crypto Wars/Assets/Scripts/cardRegistry.cs
@@ -1,20 +1,44 @@
-// // Defines a card representing a unit in the game.
-// public class Card
-// {
-//     // The type of unit the card represents.
-//     public UnitType UnitType { get; private set; }
+using System.Collections.Generic;
+using UnityEngine;
 
-//     // The name of the unit.
-//     public string Name { get; private set; }
+public class CardRegistry : MonoBehaviour
+{
+    // A list to hold all the cards
+    private List<Card> cardList = new List<Card>();
 
-//     // The resource cost to produce the unit.
-//     public int Cost { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create the cards
+        CreateCard("Python", 100, 150, 20);
+        CreateCard("Java", 200, 100, 30);
+        CreateCard("C", 150, 200, 40);
 
-//     // Constructor to create a new card with specified unit type, name, and cost.
-//     public Card(UnitType unitType, string name, int cost)
-//     {
-//         UnitType = unitType;
-//         Name = name;
-//         Cost = cost;
-//     }
-// }
+        // To test, log the names of the cards that were created
+        foreach (var card in cardList)
+        {
+            Debug.Log("Created card: " + card.getName());
+        }
+    }
+
+    // Method to create a card and add it to the list
+    void CreateCard(string name, int offense, int defense, int staminaCost)
+    {
+        
+        Sprite cardSprite = null; // Card sprite placeholder 
+
+        Card newCard = new Card(cardSprite, name);
+        newCard.setOffense(offense);
+        newCard.setDefense(defense);
+        newCard.setStaminaCost(staminaCost);
+
+        cardList.Add(newCard);
+    }
+
+    // Method to get a card by name
+    // Might be helpful
+    public Card GetCardByName(string cardName)
+    {
+        return cardList.Find(card => card.getName() == cardName);
+    }
+}
