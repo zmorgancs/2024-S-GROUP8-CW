968e18f92a7d1a56686d7b30f849ba9b47fda56c
Joseph O'Connor <venture890@gmail.com>
2024-03-23 21:41:09 -0700

    Added GUI for list of players

---
+++ Crypto Wars/Assets/Scripts/GUI/PlayerList.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerList : MonoBehaviour
+{
+    public PlayerController Controller;
+
+    // Start is called before the first frame update
+    /* Start creates all objects and components needed to create */
+    void Start()
+    {
+        TMP_FontAsset font = Resources.Load<TMP_FontAsset>("LiberationSans.ttf");
+        // create a UI 'Canvas' via code
+        GameObject PlayerList = new GameObject("PlayerList");
+        Canvas canvas = PlayerList.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        PlayerList.AddComponent<CanvasScaler>();
+        PlayerList.AddComponent<GraphicRaycaster>();
+
+        // create the panel which displays all needed information
+        GameObject panel = new GameObject("Panel");
+        panel.transform.parent = PlayerList.transform;
+        panel.AddComponent<CanvasRenderer>();
+        Image image = panel.AddComponent<Image>();
+        image.color = new Color(0, 0, 0, 0.5f);
+
+        // fix panel position to middle right of screen
+        panel.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        panel.GetComponent<RectTransform>().anchorMin = new Vector2(1, 0.5f);
+        panel.GetComponent<RectTransform>().anchorMax = new Vector2(1, 0.5f);
+        panel.GetComponent<RectTransform>().pivot = new Vector2(1, 0.5f);
+
+        // add text to panel
+        GameObject temp = new GameObject("Player1_Text");
+        TextMeshProUGUI player1 = temp.AddComponent<TextMeshProUGUI>();
+        player1.transform.parent = PlayerList.transform;
+            // align text within panel
+        player1.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        player1.GetComponent<RectTransform>().anchorMin = new Vector2(1, 0.5f);
+        player1.GetComponent<RectTransform>().anchorMax = new Vector2(1, 0.5f);
+        player1.GetComponent<RectTransform>().pivot = new Vector2(1, 0.5f);
+        player1.GetComponent<RectTransform>().sizeDelta = new Vector2(95, 50);
+            // change how text is displayed
+        player1.text = "Player 1 \t <info>";
+        player1.color = Color.blue;
+        player1.font = font;
+        player1.fontSize = 10;
+    }
+
+    // Update is called once per frame
+    /* Update() should be used to change any displayed text during the game */
+    void Update()
+    {
+        
+    }
+}
