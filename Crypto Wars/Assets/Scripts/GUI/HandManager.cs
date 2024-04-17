454dc2350c0d1b9ad530be8a12527d8b4ed369ce
Grimmblood <grimmblood@gmail.com>
2024-04-16 19:18:35 -0700

    initial implementation of HandManager

--- Crypto Wars/Assets/Scripts/GUI/HandManager.cs
+++ Crypto Wars/Assets/Scripts/GUI/HandManager.cs
@@ -13,12 +13,13 @@ using static Card;
 public class HandManager : MonoBehaviour
 {
     private static List<GameObject> Slots = new List<GameObject>();
+    private static List<string> cardNamesInHand;
     private static Hand currentPlayerHand = null;
-    private Card tempCard;
+    private Card newSlot1Card, newSlot2Card, newSlot3Card, newSlot4Card, newSlot5Card;
+    private int cardsInHandLast, newSlot1Count, newSlot2Count, newSlot3Count, newSlot4Count, newSlot5Count;
     public GameObject panel, handGrid, canvas;
-    private TextMeshProUGUI handSlot1Text, handSlot2Text, handSlot3Text;
-    private TextMeshProUGUI handSlot4Text, handSlot5Text;
-    public Boolean visible;
+    private TextMeshProUGUI handSlot1Text, handSlot2Text, handSlot3Text, handSlot4Text, handSlot5Text;
+    public Boolean visible, cardsUpdated;
     public Image image;
 
     // Start is called before the first frame update
@@ -27,7 +28,7 @@ public class HandManager : MonoBehaviour
         TMP_FontAsset font = Resources.Load<TMP_FontAsset>("LiberationSans.ttf");
         canvas = GameObject.Find("Canvas");
 
-        // currentPlayerHand = PlayerController.CurrentPlayer.GetHand();
+        currentPlayerHand = PlayerController.CurrentPlayer.GetInventory().GetHand();
 
         panel = new GameObject("Handheld Cards");
         panel.transform.parent = canvas.transform;
@@ -38,12 +39,9 @@ public class HandManager : MonoBehaviour
         EventTrigger et = panel.AddComponent<EventTrigger>();
         EventTrigger.Entry entry = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerClick;
-        // entry.callback.AddListener((data) => { ToggleVisibility(); });
         et.triggers.Add(entry);
 
         panel.GetComponent<RectTransform>().localPosition = Vector3.zero;
-        // panel.GetComponent<RectTransform>().anchorMin = new Vector2(1, 0.5f);
-        // panel.GetComponent<RectTransform>().anchorMax = new Vector2(1, 0.5f);
         panel.GetComponent<RectTransform>().pivot = new Vector2(-0.2f, 0.6f);
 
         handGrid = new GameObject("Hand Grid");
@@ -112,42 +110,62 @@ public class HandManager : MonoBehaviour
         }
 
         visible = true;
+        cardsUpdated = false;
+        cardsInHandLast = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Vector3 offsetVector = new Vector3(50, 10, 0);
+        // Code to keep the GUI elements next to the mouse at all times.
         Vector3 offsetVector = Vector3.zero;
         Vector3 offsetPosition = Input.mousePosition + offsetVector;
         panel.transform.position = offsetPosition;
 
+        if(currentPlayerHand.GetHandCards().Count != 0)
+        {
+            if(currentPlayerHand.GetHandCards().Count != cardsInHandLast)
+            {
+                cardsUpdated = true;
+            }
+        }
+
+        if(cardsUpdated)
+        {
+            ChangeDisplayedCards();
+        }
+
+        // DEBUG: Remove before shipping?
         if (Input.GetKeyDown(KeyCode.H)) {
             visible = !visible;
             ToggleHideAll();
             Debug.Log("Toggling Hand Visibility Status: " + visible);
         }
+        
+        // DEBUG: Remove before shipping?
         if (Input.GetKeyDown(KeyCode.P)) {
             int count = 4;
             PartialReveal(count);
             Debug.Log("Partially Revealing: " + count);
         }
+        
+        // DEBUG: Remove before shipping?
         if (Input.GetKeyDown(KeyCode.C)) {
             visible = false;
             ForceHideAll();
             Debug.Log("Forcing Hand Visibility Status: " + visible);
         }
+
+        // DEBUG: Remove before shipping?
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            Debug.Log("Testing Cards In Hand: " + currentPlayerHand.GetHandCards().Count);
+        }
     }
 
+    // Reveals the requested number of card names
     public void PartialReveal(int count)
     {
         image.enabled = true;
-        // for(int i = 0; i < count; i++)       If there is a more elegant solution
-        // {                                    currently does not work
-        //     if(Slots[i] != null)
-        //         GameObject temp = GameObject.Find("Hand_Slot_" + (i + 1));
-        //         temp.enabled = true;
-        // }
 
         switch(count)
         {
@@ -202,16 +220,148 @@ public class HandManager : MonoBehaviour
         handSlot4Text.enabled = false;
         handSlot5Text.enabled = false;
     }
+
+    public void ChangeDisplayedCards()
+    {
+        newSlot1Card = null;
+        newSlot2Card = null;
+        newSlot3Card = null;
+        newSlot4Card = null;
+        newSlot5Card = null;
+        newSlot1Count = 0;
+        newSlot2Count = 0;
+        newSlot3Count = 0;
+        newSlot4Count = 0;
+        newSlot5Count = 0;
+        List<string> cardNamesInHand = new List<string>();
+        // for(int i = 0; i < currentPlayerHand.GetHandCards().Count; i++)
+        // {
+            
+        // }
+        
+        // newSlot1Card = currentPlayerHand.GetHandCards()[0];
+        if(currentPlayerHand.GetHandCards().Count != 0)
+        {
+            foreach (Card oldCard in currentPlayerHand.GetHandCards())
+            {
+                if(newSlot1Card == null)
+                {
+                    if(!cardNamesInHand.Contains(oldCard.getName()))
+                    {
+                        newSlot1Card = oldCard;
+                        cardNamesInHand.Add(newSlot1Card.getName());
+                    }
+                }
+                if(newSlot1Card != null)
+                {
+                    if (cardNamesInHand.IndexOf(oldCard.getName()) == 0)
+                    {
+                        newSlot1Count++;
+                    }
+                }
+                if(newSlot2Card == null)
+                {
+                    if(!cardNamesInHand.Contains(oldCard.getName()))
+                    {
+                        newSlot2Card = oldCard;
+                        cardNamesInHand.Add(newSlot2Card.getName());
+                    }
+                }
+                if(newSlot2Card != null)
+                {
+                    if(cardNamesInHand.IndexOf(oldCard.getName()) == 1)
+                    {
+                        newSlot2Count++;
+                    }
+                }
+                if(newSlot3Card == null)
+                {
+                    if(!cardNamesInHand.Contains(oldCard.getName()))
+                    {
+                        newSlot3Card = oldCard;
+                        cardNamesInHand.Add(newSlot3Card.getName());
+                    }
+                }
+                if(newSlot3Card != null)
+                {
+                    if(cardNamesInHand.IndexOf(oldCard.getName()) == 2)
+                    {
+                        newSlot3Count++;
+                    }
+                }
+                if(newSlot4Card == null)
+                {
+                    if(!cardNamesInHand.Contains(oldCard.getName()))
+                    {
+                        newSlot4Card = oldCard;
+                        cardNamesInHand.Add(newSlot4Card.getName());
+                    }
+                }
+                if(newSlot4Card != null)
+                {
+                    if(cardNamesInHand.IndexOf(oldCard.getName()) == 3)
+                    {
+                        newSlot4Count++;
+                    }
+                }
+                if(newSlot3Card == null)
+                {
+                    if(!cardNamesInHand.Contains(oldCard.getName()))
+                    {
+                        newSlot5Card = oldCard;
+                        cardNamesInHand.Add(newSlot5Card.getName());
+                    }
+                }
+                if(newSlot5Card != null)
+                {
+                    if(cardNamesInHand.IndexOf(oldCard.getName()) == 4)
+                    {
+                        newSlot5Count++;
+                    }
+                }
+            }
+        
+            if(newSlot1Card != null)
+            {
+                handSlot1Text.text = newSlot1Count + "x " + newSlot1Card.getName();
+            }
+            if(newSlot2Card != null)
+            {
+                handSlot2Text.text = newSlot2Count + "x " + newSlot2Card.getName();
+            }
+            if(newSlot3Card != null)
+            {
+                handSlot3Text.text = newSlot3Count + "x " + newSlot3Card.getName();
+            }
+            if(newSlot4Card != null)
+            {
+                handSlot4Text.text = newSlot4Count + "x " + newSlot4Card.getName();
+            }
+            if(newSlot5Card != null)
+            {
+                handSlot5Text.text = newSlot5Count + "x " + newSlot5Card.getName();
+            }
+        }
+        else
+        {
+            ForceHideAll();
+        }
+        
+        cardsInHandLast = currentPlayerHand.GetHandCards().Count;
+        cardsUpdated = false;
+    }
     
+    // Sets the text of a given TextMeshProUGUI object
     public void SetText(string textObject, int index, string newText) {
         if(Slots[index] != null)
             Slots[index].transform.Find(textObject).GetComponent<TextMeshProUGUI>().text = newText;
     }
 
+
+    // Sets up a hand slot and sets the text values to an empty string
     public void SetupSlot(int index)
     {
         Slots.Add(gameObject.transform.Find("Hand Grid").Find("Hand_Slot_" + (index + 1)).gameObject);
-        // temp = gameObject.transform.Find("Hand Grid").Find("Hand_Slot_" + (index + 1)).gameObject;
         SetText("HandCardName", index, "");
         SetText("HandAmount", index, "");
     }
